c46a5a67e08f03e49ee4adb78d9af5a84c7e2893
itskhurram <itsmeks@gmail.com>
2020-12-04 13:20:58 -0500

    bitofai initial comingsoon

---
+++ firebase_configuration.js
@@ -0,0 +1,61 @@
+// Initialize Firebase
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+var config = {
+    apiKey: "AIzaSyBl1l1Xb_BzTIWoZEQDyFTiORVpcrQvfI0",
+    authDomain: "kaamkaajcomingsoon.firebaseapp.com",
+    databaseURL: "https://kaamkaajcomingsoon.firebaseio.com",
+    projectId: "kaamkaajcomingsoon",
+    storageBucket: "kaamkaajcomingsoon.appspot.com",
+    messagingSenderId: "316475771619",
+    appId: "1:316475771619:web:ee832c0835d5afdad193d6"
+  
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
+db.settings({
+    timestampsInSnapshots: true
+});
+
+$(document).ready(function () {
+
+    $('#notifs-form-btn').click(function (e) {
+        var is_present = false;
+        email = $('#form-email').val();
+        if (validateEmail(email)) {
+            db.collection('bitofAIEmails').get().then(function (qs) {
+                qs.forEach(function (element) {
+                    if (element.data()['ID'] == email) {
+                        is_present = true;
+                    }
+                });
+
+                if (is_present == false) {
+                    db.collection("bitofAIEmails").add({
+                        'ID': email,
+                    })
+                        .then(function (docRef) {
+                            console.log("Document written with ID: ", docRef.id);
+                            alert('Email has been added. Thank you for your interest.')
+                            $("#notifs-form-btn").text("Notified!");
+                        })
+                        .catch(function (error) {
+                            console.error("Error adding document: ", error);
+                        });
+                }
+                else
+                {
+                    alert('Email already has been added. Thank you for your interest.');
+                }
+            });
+
+        }
+        else {
+            alert('Invalid Email');
+        }
+    });
+
+});
